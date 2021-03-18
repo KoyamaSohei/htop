7580cd9a4898f933bda0c956208c3d9593d0271b
KoyamaSohei <koyamaso0309@gmail.com>
2021-03-19 00:31:32 +0900

    add user col

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/user"
 	"strconv"
 	"strings"
+	"syscall"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
@@ -37,6 +40,23 @@ func getCommand(pid int) (string, error) {
 	return string(b), nil
 }
 
+func getUser(pid int) (string, error) {
+	info, err := os.Stat(fmt.Sprintf("/proc/%d", pid))
+	if err != nil {
+		return "", err
+	}
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return "", fmt.Errorf("failed to get uid or gid from pid")
+	}
+	uid := stat.Uid
+	u, err := user.LookupId(fmt.Sprintf("%d", uid))
+	if err != nil {
+		return "", err
+	}
+	return u.Name, nil
+}
+
 func trim(str string) string {
 	if len(str) > MAX_LENGTH {
 		str = str[:MAX_LENGTH]
@@ -62,14 +82,21 @@ func main() {
 	plist.Border = false
 	plist.TextStyle = ui.NewStyle(ui.ColorYellow)
 	plist.WrapText = true
-	plist.SetRect(0, 0, 25, 40)
+	plist.SetRect(0, 0, 8, 40)
+
+	ulist := widgets.NewList()
+	ulist.Title = "USER"
+	ulist.Border = false
+	ulist.TextStyle = ui.NewStyle(ui.ColorYellow)
+	ulist.WrapText = true
+	ulist.SetRect(8, 0, 24, 40)
 
 	clist := widgets.NewList()
 	clist.Title = "Command"
 	clist.Border = false
 	clist.TextStyle = ui.NewStyle(ui.ColorYellow)
 	clist.WrapText = true
-	clist.SetRect(25, 0, 60, 40)
+	clist.SetRect(24, 0, 56, 40)
 
 	pids, err := getPids()
 	if err != nil {
@@ -77,6 +104,11 @@ func main() {
 	}
 	for _, p := range pids {
 		plist.Rows = append(plist.Rows, fmt.Sprintf("%d", p))
+		u, err := getUser(p)
+		if err != nil {
+			log.Fatalf("failed to get user: %v", err)
+		}
+		ulist.Rows = append(ulist.Rows, u)
 		c, err := getCommand(p)
 		if err != nil {
 			log.Fatalf("failed to get command: %v", err)
@@ -85,6 +117,7 @@ func main() {
 	}
 
 	ui.Render(plist)
+	ui.Render(ulist)
 	ui.Render(clist)
 
 	uiEvents := ui.PollEvents()
@@ -95,24 +128,31 @@ func main() {
 			return
 		case "j", "<Down>":
 			plist.ScrollDown()
+			ulist.ScrollDown()
 			clist.ScrollDown()
 		case "k", "<Up>":
 			plist.ScrollUp()
+			ulist.ScrollUp()
 			clist.ScrollUp()
 		case "<C-d>":
 			plist.ScrollHalfPageDown()
+			ulist.ScrollHalfPageDown()
 			clist.ScrollHalfPageDown()
 		case "<C-u>":
 			plist.ScrollHalfPageUp()
+			ulist.ScrollHalfPageUp()
 			clist.ScrollHalfPageUp()
 		case "<C-f>":
 			plist.ScrollPageDown()
+			ulist.ScrollPageDown()
 			clist.ScrollPageDown()
 		case "<C-b>":
 			plist.ScrollPageUp()
+			ulist.ScrollPageUp()
 			clist.ScrollPageUp()
 		}
 		ui.Render(plist)
+		ui.Render(ulist)
 		ui.Render(clist)
 	}
 
