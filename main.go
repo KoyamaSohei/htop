9d4b9fff20cc409fa8cf56790ef24e38ec53fe2b
KoyamaSohei <koyamaso0309@gmail.com>
2021-03-18 23:45:07 +0900

    add command col

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"io/ioutil"
 	"log"
 	"strconv"
+	"strings"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 )
+
+const MAX_LENGTH = 30
 
 func getPids() ([]int, error) {
 	files, err := ioutil.ReadDir("/proc")
@@ -26,6 +29,28 @@ func getPids() ([]int, error) {
 	return pids, nil
 }
 
+func getCommand(pid int) (string, error) {
+	b, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func trim(str string) string {
+	if len(str) > MAX_LENGTH {
+		str = str[:MAX_LENGTH]
+	}
+	s := strings.TrimRightFunc(str, func(c rune) bool {
+		//In windows newline is \r\n
+		return c == '\r' || c == '\n'
+	})
+	if len(s) > MAX_LENGTH {
+		s = s[:MAX_LENGTH] + "..."
+	}
+	return s
+}
+
 func main() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initiaplistze termui: %v", err)
@@ -39,15 +64,29 @@ func main() {
 	plist.WrapText = true
 	plist.SetRect(0, 0, 25, 40)
 
+	clist := widgets.NewList()
+	clist.Title = "Command"
+	clist.Border = false
+	clist.TextStyle = ui.NewStyle(ui.ColorYellow)
+	clist.WrapText = true
+	clist.SetRect(25, 0, 60, 40)
+
 	pids, err := getPids()
 	if err != nil {
 		log.Fatalf("failed to get pid: %v", err)
 	}
 	for _, p := range pids {
 		plist.Rows = append(plist.Rows, fmt.Sprintf("%d", p))
+		c, err := getCommand(p)
+		if err != nil {
+			log.Fatalf("failed to get command: %v", err)
+		}
+		clist.Rows = append(clist.Rows, trim(c))
 	}
 
 	ui.Render(plist)
+	ui.Render(clist)
+
 	uiEvents := ui.PollEvents()
 	for {
 		e := <-uiEvents
@@ -56,18 +95,25 @@ func main() {
 			return
 		case "j", "<Down>":
 			plist.ScrollDown()
+			clist.ScrollDown()
 		case "k", "<Up>":
 			plist.ScrollUp()
+			clist.ScrollUp()
 		case "<C-d>":
 			plist.ScrollHalfPageDown()
+			clist.ScrollHalfPageDown()
 		case "<C-u>":
 			plist.ScrollHalfPageUp()
+			clist.ScrollHalfPageUp()
 		case "<C-f>":
 			plist.ScrollPageDown()
+			clist.ScrollPageDown()
 		case "<C-b>":
 			plist.ScrollPageUp()
+			clist.ScrollPageUp()
 		}
 		ui.Render(plist)
+		ui.Render(clist)
 	}
 
 }
