ab88e35ac8070cd56b2b535b52c2419bfafba774
KoyamaSohei <koyamaso0309@gmail.com>
2021-03-18 23:19:01 +0900

    add termui

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KoyamaSohei/htop
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/stretchr/testify v1.7.0
+)
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"strconv"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 func getPids() ([]int, error) {
@@ -23,11 +27,46 @@ func getPids() ([]int, error) {
 }
 
 func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	li := widgets.NewList()
+	li.Title = "PID"
+	li.TextStyle = ui.NewStyle(ui.ColorYellow)
+	li.WrapText = true
+	li.SetRect(0, 0, 25, 40)
+
 	pids, err := getPids()
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to get pid: %v", err)
 	}
 	for _, p := range pids {
-		fmt.Println(p)
+		li.Rows = append(li.Rows, fmt.Sprintf("%d", p))
 	}
+
+	ui.Render(li)
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			li.ScrollDown()
+		case "k", "<Up>":
+			li.ScrollUp()
+		case "<C-d>":
+			li.ScrollHalfPageDown()
+		case "<C-u>":
+			li.ScrollHalfPageUp()
+		case "<C-f>":
+			li.ScrollPageDown()
+		case "<C-b>":
+			li.ScrollPageUp()
+		}
+		ui.Render(li)
+	}
+
 }
